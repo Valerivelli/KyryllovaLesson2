b1f50611bfc8d07331bff9f63bbfd6f5d76272d4
velli <artuuutvelli@gmail.com>
2021-01-12 16:27:47 +0200

    Initial commit

---
+++ src/main/java/com/kyryllova/homeworks/hw2/HeronsFormula.java
@@ -0,0 +1,18 @@
+package com.kyryllova.homeworks.hw2;
+
+import java.util.Scanner;
+
+public class HeronsFormula {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        int p = (a + b + c)/2;
+
+        System.out.println("S = " + Math.sqrt(p*(p-a)*(p-b)*(p-c)));
+
+    }
+}
